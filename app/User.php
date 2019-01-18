f65aa755d8a420720171dd8166e1d7f5ad74203d
cysonya <cysonya@gmail.com>
2019-01-18 17:46:31 -0500

    Add make:auth

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name', 'last_name', 'email', 'password',
     ];
 
     /**
@@ -27,4 +27,13 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+      * Get user full name
+      *
+      * @return string
+      */
+    public function name() {
+        return $this->first_name . ' ' . $this->last_name;
+    }
 }
