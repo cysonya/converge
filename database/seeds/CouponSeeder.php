59695e1b67c33b50075bd7ed045ffa7d4f192004
cysonya <cysonya@gmail.com>
2019-06-05 18:25:58 -0400

    Update seeder promo code

--- database/seeds/CouponSeeder.php
+++ database/seeds/CouponSeeder.php
@@ -12,7 +12,7 @@ class CouponSeeder extends Seeder
     public function run()
     {
         \App\Coupon::create([
-            'code' => "speaker19",
+            'code' => "CONVERGE2019",
             'type' => 'package_off',
             'expiry_date' => Carbon\Carbon::createFromFormat('Y-m-d', '2019-08-04')
         ]);    
