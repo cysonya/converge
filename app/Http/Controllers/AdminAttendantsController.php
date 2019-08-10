656a7ee1a490aa954105e14a421a5038b42a7e0b
cysonya <cysonya@gmail.com>
2019-08-09 22:19:05 -0400

    Remove email from export

--- app/Http/Controllers/AdminAttendantsController.php
+++ app/Http/Controllers/AdminAttendantsController.php
@@ -27,7 +27,6 @@ class AdminAttendantsController extends Controller
         $columns = [
             'First Name', 
             'Last Name', 
-            'Email',
             'Age Group', 
             'Housing', 
             'Affiliate',
@@ -44,7 +43,6 @@ class AdminAttendantsController extends Controller
                 $row = [
                     $attendant->first_name,
                     $attendant->last_name,
-                    $attendant->email,
                     $attendant->group->description,
                     $attendant->package->title,
                     $attendant->custom_properties['affiliate'],
